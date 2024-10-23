d423cb501c8fe7a819958b56a6187708c014de3f
I2A4CCC <bissac280@gmail.com>
2024-10-23 18:02:48 -0400

    Reposity #2

---
+++ Class.py
@@ -0,0 +1,39 @@
+# Write code below 💖
+
+# Write code below 💖
+
+class Pokemon():
+  def __init__(self, entry, name, types, description, is_caught):
+    self.entry = entry
+    self.name = name
+    self.types = types
+    self.description = description
+    self.is_caught = is_caught
+  def speak(self):
+    print(self.name +" "+ self.name)
+  def display_details(self):
+    if self.is_caught == True:
+          print('Entry: ' + str(self.entry))
+          print('Name: ' + str(self.name))
+          print('Type: ' + str(self.types))
+          print('Description: ' + str(self.description))
+          print(str(self.name) + ' has been caught')
+    else:
+          print('Entry: ' + str(self.entry))
+          print('Name: ' + str(self.name))
+          print('Type: ' + str(self.types))
+          print('Description: ' + str(self.description))
+          print(str(self.name) + ' has not been caught')
+
+Bulbasaur = Pokemon(1, 'Bulbasaur', 'Grass', 'It uses the nutrients that are packed into the seed', True)
+Bulbasaur.speak()
+Bulbasaur.display_details()
+
+Charizard = Pokemon(6, 'Charizard', 'Fire', 'It breathes flames from its mouth', False)
+Charizard.speak()
+Charizard.display_details()
+
+Metapod = Pokemon(11, 'Metapod', 'Bug', 'it can only harden, remains motionless', True)
+Metapod.speak()
+Metapod.display_details()
+
