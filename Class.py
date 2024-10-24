a17fe2526904a43db085366c40c38c59bbbb0c63
I2A4CCC <bissac280@gmail.com>
2024-10-24 12:26:35 -0400

    updates

--- Class.py
+++ Class.py
@@ -17,23 +17,26 @@ class Pokemon():
           print('Name: ' + str(self.name))
           print('Type: ' + str(self.types))
           print('Description: ' + str(self.description))
-          print(str(self.name) + ' has been caught')
+          print(str(self.name) + ' has been caught!')
     else:
           print('Entry: ' + str(self.entry))
           print('Name: ' + str(self.name))
           print('Type: ' + str(self.types))
           print('Description: ' + str(self.description))
-          print(str(self.name) + ' has not been caught')
+          print(str(self.name) + ' has not been caught ;(')
 
 Bulbasaur = Pokemon(1, 'Bulbasaur', 'Grass', 'It uses the nutrients that are packed into the seed', True)
 Bulbasaur.speak()
 Bulbasaur.display_details()
+print("===================")
 
 Charizard = Pokemon(6, 'Charizard', 'Fire', 'It breathes flames from its mouth', False)
 Charizard.speak()
 Charizard.display_details()
+print("===================")
 
 Metapod = Pokemon(11, 'Metapod', 'Bug', 'it can only harden, remains motionless', True)
 Metapod.speak()
 Metapod.display_details()
+print("===================")
 
